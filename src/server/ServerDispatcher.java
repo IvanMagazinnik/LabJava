8fcbb451ab1fe0bcf31bdca59ba03817ee9324ff
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2017-04-06 21:51:18 +0300

    added log, some refactoring

--- src/server/ServerDispatcher.java
+++ src/server/ServerDispatcher.java
@@ -9,32 +9,34 @@ import java.util.logging.*;
 public class ServerDispatcher extends Thread
 {
     private static Logger log = Logger.getLogger(Server.class.getName());
-    private Vector mClients = new Vector();
+    private Vector<ClientInfo> mClients = new Vector<>();
     private boolean enabled = true;
     /**
      * Adds given client to the server's client list.
      */
-    public synchronized void addClient(ClientInfo aClientInfo)
+    synchronized void addClient(ClientInfo aClientInfo)
     {
         mClients.add(aClientInfo);
+        log.info("Added client: " + aClientInfo);
     }
 
     /**
      * Deletes given client from the server's client list
      * if the client is in the list.
      */
-    public synchronized void deleteClient(ClientInfo aClientInfo)
+    synchronized void deleteClient(ClientInfo aClientInfo)
     {
         int clientIndex = mClients.indexOf(aClientInfo);
         if (clientIndex != -1)
             mClients.removeElementAt(clientIndex);
+            log.info("Removed client: " + aClientInfo);
     }
 
     private synchronized void sendGameStatusToAll(String aMessage)
     {
-        for (int i=0; i<mClients.size(); i++) {
-            ClientInfo clientInfo = (ClientInfo) mClients.get(i);
+        for (ClientInfo clientInfo: mClients) {
             clientInfo.mClientSender.sendMessage(aMessage);
+            log.info("Message " + aMessage + " sent to client: " + clientInfo);
         }
     }
 
