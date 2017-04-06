febb0e3f2ad33bf0cd1666063e6d00b66ea6e659
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2017-04-06 19:47:18 +0300

    Initial commit

---
+++ src/client/client.java
@@ -0,0 +1,7 @@
+package client;
+
+/**
+ * Created by ivan on 06.04.2017.
+ */
+public class client {
+}
---
+++ src/server/server.java
@@ -0,0 +1,7 @@
+package server;
+
+/**
+ * Created by ivan on 06.04.2017.
+ */
+public class server {
+}
