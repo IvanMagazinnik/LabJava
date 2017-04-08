b7dffbfd1d9b5c0e8319225e89e44e3f900ed8dd
Ivan Magazinnik <magazinnik.ivan@gmail.com>
2017-04-08 11:40:32 +0300

    Add start function

--- src/server/server.java
+++ src/server/server.java
@@ -4,4 +4,8 @@ package server;
  * Created by ivan on 08.04.2017.
  */
 public class server {
+    public static void main(String[] args) {
+        ServerModel model = new ServerModel();
+        model.start();
+    }
 }
